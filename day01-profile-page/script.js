05cca4a20a17621d1cbdd430ca0e4475fcafdb8d
justinpeter <justinpeter0302@gmail.com>
2026-06-14 16:59:29 +0800

    为第一天练习添加教学注释

--- day01-profile-page/script.js
+++ day01-profile-page/script.js
@@ -1,5 +1,8 @@
+// 找到 HTML 中 id 为 themeButton 的按钮。
 const themeButton = document.querySelector("#themeButton");
 
+// 当用户点击按钮时，执行里面的代码。
 themeButton.addEventListener("click", () => {
+  // toggle 会自动切换 dark class：没有就添加，有就移除。
   document.body.classList.toggle("dark");
 });
